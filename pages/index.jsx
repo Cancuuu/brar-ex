9709eda2961322f5a5dd3f705f5561260309e931
Cancu <juancruz.cancu@gmail.com>
2022-02-04 02:15:39 -0300

    emojis added better ux

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,51 @@
-import React, {useState, useEffect} from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
   const [realAmount, setRealAmount] = useState(0);
   const [arsAmount, setArsAmount] = useState(0);
 
   const exchangeReal = () => {
-    const real = realAmount;  
+    const real = realAmount;
     const realToUsd = real / 5.28;
     const usdToArs = realToUsd * 183; //masomenos
-    setArsAmount(usdToArs.toFixed(2));
-  }
-
+    setArsAmount(usdToArs);
+  };
 
   useEffect(() => {
     exchangeReal();
-  }, [realAmount, arsAmount])
-
-
+  }, [realAmount, arsAmount]);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className={styles.ex_container}>
-          <p>BRL to ARS</p>
-          <input placeholder='Real Brasilero' type="text" value={realAmount} onChange={(e) => setRealAmount(e.target.value)} />
-          <input placeholder='Peso Argentino' type="text" value={arsAmount} onChange={(e) => setArsAmount(e.target.value)} />
+        <div>
+          <p className={styles.title}>BRL to ARS</p>
+          <div className={styles.input_container}>
+            <label className={styles.label}>🇧🇷</label>
+            <input
+              className={styles.input}  
+              placeholder="Real Brasilero"
+              type="number"
+              value={realAmount}
+              onChange={(e) => setRealAmount(e.target.value)}
+            />
+          </div>
+          <div className={styles.input_container}>
+            <label className={styles.label}>🇦🇷</label>
+            <input
+            className={styles.input}
+            placeholder="Peso Argentino"
+            type="number"
+            value={arsAmount.toFixed(2)}
+            onChange={(e) => setArsAmount(e.target.value)}
+          />
+          </div>
+
         </div>
       </main>
-
     </div>
-  )
+  );
 }
